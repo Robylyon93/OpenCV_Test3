3c28d4ebf065de089e6366e0f3708333d0f3ceab
Simone Zini <simonez93@gmail.com>
2016-12-26 23:58:06 +0100

    Aggiunta activity per le opzioni e modificata grafica generale

--- app/src/main/java/bicoccalab/opencv_test3/OptionActivity.java
+++ app/src/main/java/bicoccalab/opencv_test3/OptionActivity.java
@@ -2,20 +2,20 @@ package bicoccalab.opencv_test3;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 import android.view.Window;
 import android.widget.ImageView;
 
-public class DisplayActivity extends AppCompatActivity {
+public class OptionActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Uri photoUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display);
+        setContentView(R.layout.activity_option);
 
         imageView = (ImageView) findViewById(R.id.imageViewDisplay);
         Intent i = getIntent();
@@ -23,12 +23,10 @@ public class DisplayActivity extends AppCompatActivity {
         imageView.setImageURI(photoUri);
     }
 
-    public void test(View v) {
-        Intent optionsIntent = new Intent(this, OptionActivity.class);
-        optionsIntent.putExtra("photoUri", photoUri.toString());
-
-        optionsIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); //impedisce che ci sia l'animazione di slide
-        startActivity(optionsIntent);
+    // aggiunto per eliminare la transizione alla pressione del tasto back
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
     }
-
 }
