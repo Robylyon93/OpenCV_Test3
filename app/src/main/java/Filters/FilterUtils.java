552f5558728ff2bcf454715d099bd7b94cdeae1c
TheZino <simonez93@gmail.com>
2017-01-05 17:17:16 +0100

    Cartoon e Save: non funzionanti

--- app/src/main/java/Filters/FilterUtils.java
+++ app/src/main/java/Filters/FilterUtils.java
@@ -1,8 +1,11 @@
 package Filters;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.photo.Photo;
@@ -10,6 +13,10 @@ import org.opencv.photo.Photo;
 import static org.opencv.android.Utils.bitmapToMat;
 import static org.opencv.android.Utils.matToBitmap;
 import static org.opencv.core.CvType.CV_8U;
+import static org.opencv.imgproc.Imgproc.ADAPTIVE_THRESH_MEAN_C;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY_INV;
+import static org.opencv.imgproc.Imgproc.resize;
 
 public class FilterUtils {
 
@@ -29,17 +36,48 @@ public class FilterUtils {
 
     public static Bitmap stylizationApply(Bitmap image) {
 
+        // Cartoon Style
+        // Ho ancora dei problemi con il bitwise and, mi da errore sulle dimensioni delle mat ma non capisco il perchè...
+
         Mat imageMat = new Mat();
         bitmapToMat(image, imageMat);
+        Mat imageGray = new Mat();
         Mat dst = new Mat();
+        Mat edges = new Mat();
+        Mat mask = new Mat();
 
+        // convert to grayscale and apply median blur
+        Imgproc.cvtColor(imageMat,imageGray, Imgproc.COLOR_BGR2GRAY);
 
-        imageMat.convertTo(imageMat, CV_8U);
+        Mat imageBlur = new Mat();
+        Imgproc.medianBlur(imageGray,imageBlur,3);
 
-        Photo.stylization(imageMat, dst);
+        // detect and enhance edges
+        Imgproc.adaptiveThreshold(imageBlur,edges, 255, ADAPTIVE_THRESH_MEAN_C,
+                                THRESH_BINARY, 9, 2);
+
+        // convert back to color so that it can be bit-ANDed with color image
+        Mat edges_rgb = new Mat();
+        Imgproc.cvtColor(edges, edges_rgb, Imgproc.COLOR_GRAY2RGB);
+        resize(edges_rgb,edges_rgb, imageMat.size());
+        Log.i("Filters", edges_rgb.size().toString() + "," + imageMat.size().toString());
+        Core.bitwise_and(edges_rgb,imageMat, dst);
+
 
         matToBitmap(dst, image);
 
+        return image;
+    }
+
+    public static Bitmap pencilApply(Bitmap image) {
+        Mat imageMat = new Mat();
+        bitmapToMat(image, imageMat);
+        Mat dst = new Mat();
+        Mat dstGray = new Mat();
+
+        Photo.pencilSketch(imageMat,dstGray,dst,60,0.02f,0.03f);
+
+        matToBitmap(dst, image);
         return image;
     }
 }
--- app/src/main/java/bicoccalab/opencv_test3/DisplayActivity.java
+++ app/src/main/java/bicoccalab/opencv_test3/DisplayActivity.java
@@ -5,12 +5,17 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import Filters.FilterUtils;
 
@@ -18,7 +23,7 @@ public class DisplayActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Uri photoUri;
-    private Uri newUri;
+    private Bitmap originalImg;
     private Bitmap newImage;
 
     @Override
@@ -31,38 +36,64 @@ public class DisplayActivity extends AppCompatActivity {
         Intent i = getIntent();
         photoUri = Uri.parse(i.getExtras().getString("photoUri"));
         imageView.setImageURI(photoUri);
+        originalImg = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
     }
 
     public void onClick(View v) throws FileNotFoundException {
 
-        // In base all'id del bottone premuto vengono chiamate i corrispondenti filtri
-        if(v.getId() == R.id.grayScale) {
+        if(v.getId() == R.id.saveImg){
+            SaveImage();
+        }
+        else {
 
-            newImage = FilterUtils.grayApply(((BitmapDrawable) imageView.getDrawable()).getBitmap());
+            // In base all'id del bottone premuto vengono chiamate i corrispondenti filtri
+            if (v.getId() == R.id.grayScale) {
+
+                newImage = FilterUtils.grayApply(originalImg);
+
+            } else if (v.getId() == R.id.stylization) {
+
+                newImage = FilterUtils.stylizationApply(originalImg);
+
+            } else if (v.getId() == R.id.pencil) {
+                newImage = FilterUtils.pencilApply(originalImg);
+            }
+
+            imageView.setImageBitmap(newImage);
+//
+//          String destFolder = getCacheDir().getAbsolutePath();
+//
+//          FileOutputStream out = new FileOutputStream(destFolder + "/temp_image.png");
+//          newImage.compress(Bitmap.CompressFormat.PNG, 100, out);
+//
+//          String newPath = destFolder + "/temp_image.png";
+//
+//          Intent optionsIntent = new Intent(this, OptionActivity.class);
+//          optionsIntent.putExtra("photoUri", newPath);
+//
+//          optionsIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); //impedisce che ci sia l'animazione di slide
+//          startActivity(optionsIntent);
 
         }
-        else if(v.getId() == R.id.stylization) {
-
-            newImage = FilterUtils.stylizationApply(((BitmapDrawable) imageView.getDrawable()).getBitmap());
-
-        }
-
-        imageView.setImageBitmap(newImage);
-//
-//        String destFolder = getCacheDir().getAbsolutePath();
-//
-//        FileOutputStream out = new FileOutputStream(destFolder + "/temp_image.png");
-//        newImage.compress(Bitmap.CompressFormat.PNG, 100, out);
-//
-//        String newPath = destFolder + "/temp_image.png";
-//
-//        Intent optionsIntent = new Intent(this, OptionActivity.class);
-//        optionsIntent.putExtra("photoUri", newPath);
-//
-//        optionsIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION); //impedisce che ci sia l'animazione di slide
-//        startActivity(optionsIntent);
-
-
     }
 
+
+    private void SaveImage() {
+
+        File myDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "ApeDemo");
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fname = "JPEG_" + timeStamp + "_APE_";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            newImage.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
